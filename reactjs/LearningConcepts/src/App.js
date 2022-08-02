023936f4eb96d3183eb60ce8f21a153d7c6ba38a
kishor kunal <kkunal@ciena.com>
2022-08-02 09:36:40 +0530

    first commit

---
+++ reactjs/LearningConcepts/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+import GridExample from './component/css.grid'
+
+function App() {
+  return (
+    <GridExample />
+  );
+}
+
+export default App;
---
+++ reactjs/LearningConcepts/src/component/css.grid.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './css.grid.css'
+
+const GridExample = () => {
+    return (
+        <div class="container">
+            <div class="item1">item</div>
+            <div class="item2">item</div>
+            <div class="item3">item</div>
+            <div class="item4">item</div>
+            <div class="item5">item</div>
+            <div class="item6">item long </div>
+            <div class="item7">item</div>
+            <div class="item8">item</div>
+            <div class="item9">item long longer</div>
+            {/* <div class="item10">item</div>
+            <div class="item11">item</div>
+            <div class="item12">item</div>
+            <div class="item13">item</div>
+            <div class="item14">item</div>
+            <div class="item15">item</div> */}
+        </div>
+    )
+}
+
+export default GridExample
